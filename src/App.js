e6834f55054d569190c901a47a0b8f069f76b328
Wojtek Książek <wojtek99955@gmail.com>
2022-05-04 11:29:26 +0200

    Router update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import MainPage from "./components/MainPage.js/MainPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MainPage />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
     </div>
   );
 }
