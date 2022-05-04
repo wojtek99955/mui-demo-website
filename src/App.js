98957ceae656390f95e87f7e05c2a38e7cc91b2e
Wojtek Książek <wojtek99955@gmail.com>
2022-05-04 11:28:05 +0200

    MainPage added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import MainPage from "./components/MainPage.js/MainPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MainPage />
     </div>
   );
 }
---
+++ src/components/MainPage.js/MainPage.js
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+import Container from "@mui/material/Container";
+
+function MainPage() {
+  return (
+    <Container>
+      <Typography variant="h2" align="center" sx={{ marginTop: "3rem" }}>
+        Welcome
+      </Typography>
+    </Container>
+  );
+}
+
+export default MainPage;
