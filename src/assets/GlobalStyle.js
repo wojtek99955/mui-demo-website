4948f747e6724bd45ee05a09c496a6d498561617
Wojtek Książek <wojtek99955@gmail.com>
2022-05-05 11:52:45 +0200

    Font update

--- src/assets/GlobalStyle.js
+++ src/assets/GlobalStyle.js
@@ -1,9 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap');
+
 *{
     margin:0;
     padding:0;
     box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
 }
 `;
