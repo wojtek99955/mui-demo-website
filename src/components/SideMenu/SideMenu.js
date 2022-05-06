46e54fa867715e08cf170f9a5c002c821723d2aa
Wojtek Książek <wojtek99955@gmail.com>
2022-05-06 09:50:11 +0200

    SideMenu list items added

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -8,24 +8,36 @@ import {
 import styled from "styled-components";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import HelpIcon from "@mui/icons-material/Help";
+import SettingsIcon from "@mui/icons-material/Settings";
+
+const links = [
+  {
+    text: "Dashboard",
+    icon: <DashboardIcon />,
+  },
+  {
+    text: "Settings",
+    icon: <SettingsIcon />,
+  },
+  {
+    text: "Help",
+    icon: <HelpIcon />,
+  },
+];
 
 function SideMenu() {
   return (
     <>
       <Drawer variant="permanent" anchor="left" sx={{ width: "6rem" }}>
         <List>
-          <ListItem button>
-            <ListItemIcon>
-              <DashboardIcon />
-            </ListItemIcon>
-            <ListItemText primary="Dashboard" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <HelpIcon />
-            </ListItemIcon>
-            <ListItemText primary="Help" />
-          </ListItem>
+          {links.map((link) => {
+            return (
+              <ListItem button>
+                <ListItemIcon>{link.icon}</ListItemIcon>
+                <ListItemText primary={link.text} />
+              </ListItem>
+            );
+          })}
         </List>
       </Drawer>
     </>
