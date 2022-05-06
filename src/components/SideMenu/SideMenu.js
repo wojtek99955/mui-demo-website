d6f3b3086efaf20acfe57e1ee93a2a4f873a77ed
Wojtek Książek <wojtek99955@gmail.com>
2022-05-06 09:40:56 +0200

    SideMenu list items added

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -6,12 +6,27 @@ import {
   ListItemText,
 } from "@mui/material";
 import styled from "styled-components";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import HelpIcon from "@mui/icons-material/Help";
 
 function SideMenu() {
   return (
     <>
-      <Drawer variant="permanent" anchor="left">
-        <h1>dd</h1>
+      <Drawer variant="permanent" anchor="left" sx={{ width: "6rem" }}>
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <HelpIcon />
+            </ListItemIcon>
+            <ListItemText primary="Help" />
+          </ListItem>
+        </List>
       </Drawer>
     </>
   );
