8c461073500c8fa9d82c4960271ddf61052b5850
Wojtek Książek <wojtek99955@gmail.com>
2022-05-06 09:03:59 +0200

    Profile mui update

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,45 +3,38 @@ import { Box, Typography } from "@mui/material";
 import { Button } from "@mui/material";
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
+import { Card } from "@mui/material";
 
 const Container = styled.section``;
-const Wrapper = styled.div`
-  margin: auto;
-  margin-top: 5rem;
-  max-width: 600px;
-  border: 1px solid grey;
-  padding: 2rem;
-`;
+
 const StyledLink = styled(Link)`
   text-decoration: none;
 `;
 
 function Profile() {
   return (
-    <Container>
-      <Wrapper>
-        <Typography variant="h3" component="h1" align="center">
-          Your Profile
-        </Typography>
-        <Stack direction="row" justifyContent="space-around">
-          <Typography>Name</Typography>
-          <Typography>Surname</Typography>
-        </Stack>
-        <Stack direction="row" justifyContent="space-around">
-          <Typography>Age</Typography>
-          <Typography>Address</Typography>
-        </Stack>
-        <Stack direction="row" justifyContent="space-around">
-          <Typography>Zip Code</Typography>
-          <Typography>City</Typography>
-        </Stack>
-        <Box textAlign="center">
-          <StyledLink to="/edit-profile">
-            <Button variant="contained">Update</Button>
-          </StyledLink>
-        </Box>
-      </Wrapper>
-    </Container>
+    <Card sx={{ maxWidth: "600px", margin: "auto", marginTop: "1rem" }}>
+      <Typography variant="h3" component="h1" align="center">
+        Your Profile
+      </Typography>
+      <Stack direction="row" justifyContent="space-around">
+        <Typography>Name</Typography>
+        <Typography>Surname</Typography>
+      </Stack>
+      <Stack direction="row" justifyContent="space-around">
+        <Typography>Age</Typography>
+        <Typography>Address</Typography>
+      </Stack>
+      <Stack direction="row" justifyContent="space-around">
+        <Typography>Zip Code</Typography>
+        <Typography>City</Typography>
+      </Stack>
+      <Box textAlign="center">
+        <StyledLink to="/edit-profile">
+          <Button variant="contained">Update</Button>
+        </StyledLink>
+      </Box>
+    </Card>
   );
 }
 
