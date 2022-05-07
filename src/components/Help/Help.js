496ddffad503701370b261b7fb360bf7b31167a0
Wojtek Książek <wojtek99955@gmail.com>
2022-05-07 15:51:44 +0200

    Help upate

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -1,5 +1,16 @@
+import { Container, Typography, Button, Box } from "@mui/material";
+
 function Help() {
-  return <div>Help</div>;
+  return (
+    <Container>
+      <Typography variant="h3" textAlign="center" mt={2}>
+        Support
+      </Typography>
+      <Box textAlign="center" mt={4}>
+        <Button variant="contained">contact support</Button>
+      </Box>
+    </Container>
+  );
 }
 
 export default Help;
