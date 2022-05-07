c8c5a4b4b82c35564637f9cfe24528e90e1e3623
Wojtek Książek <wojtek99955@gmail.com>
2022-05-07 14:54:50 +0200

    Settings mui update

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,5 +1,14 @@
+import { Container, Typography, Switch } from "@mui/material";
+
 function Settings() {
-  return <div>Settings</div>;
+  return (
+    <Container>
+      <Typography variant="h4">Settings</Typography>
+      <Typography variant="h5">Account</Typography>
+      <Typography variant="h5">Preferences</Typography>
+      <Switch />
+    </Container>
+  );
 }
 
 export default Settings;
