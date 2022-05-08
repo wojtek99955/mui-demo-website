da6867fa68f369f2aa60d0188ed827055da427a5
Wojtek Książek <wojtek99955@gmail.com>
2022-05-08 19:05:14 +0200

    Profile userData added

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,6 +4,8 @@ import { Button } from "@mui/material";
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 import { Card } from "@mui/material";
+import { useContext } from "react";
+import { Context } from "../../ContextProvider";
 
 const Container = styled.section``;
 
@@ -12,22 +14,22 @@ const StyledLink = styled(Link)`
 `;
 
 function Profile() {
+  const context = useContext(Context);
   return (
     <Card sx={{ maxWidth: "600px", margin: "auto", marginTop: "1rem" }}>
       <Typography variant="h3" component="h1" align="center">
         Your Profile
       </Typography>
       <Stack direction="row" justifyContent="space-around">
-        <Typography>Name</Typography>
-        <Typography>Surname</Typography>
+        <Typography>{context.userData.name}</Typography>
+        <Typography>{context.userData.surname}</Typography>
       </Stack>
       <Stack direction="row" justifyContent="space-around">
-        <Typography>Age</Typography>
-        <Typography>Address</Typography>
+        <Typography>{context.userData.address}</Typography>
       </Stack>
       <Stack direction="row" justifyContent="space-around">
-        <Typography>Zip Code</Typography>
-        <Typography>City</Typography>
+        <Typography>{context.userData.zipCode}</Typography>
+        <Typography>{context.userData.city}</Typography>
       </Stack>
       <Box textAlign="center">
         <StyledLink to="/edit-profile">
