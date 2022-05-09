bc8a2cf691e3870ce2092b45bd13b7cf94ac066b
Wojtek Książek <wojtek99955@gmail.com>
2022-05-09 17:56:06 +0200

    AddPost formik added

--- src/components/Posts/AddPost.js
+++ src/components/Posts/AddPost.js
@@ -3,29 +3,53 @@ import { useContext } from "react";
 import { Context } from "../../ContextProvider";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import styled from "styled-components";
+import { Formik, Form, Field } from "formik";
+import Posts from "./Posts";
 
 const UserIcon = styled(AccountCircleOutlinedIcon)``;
+
 function AddPost() {
   const ctx = useContext(Context);
   const {
     userData: { name, surname },
   } = ctx;
+
+  const initialValues = {
+    name,
+    surname,
+    text: "",
+  };
+  const onSubmit = (values) => {
+    ctx.setPosts([values, ...ctx.posts]);
+  };
+  console.log(ctx.posts);
   return (
     <Card sx={{ width: "550px", margin: " 1rem auto", padding: "1rem" }}>
       <Stack direction="row" spacing={2}>
         <UserIcon />
         <Typography>{`${name} ${surname}`}</Typography>
       </Stack>
-      <TextField
-        multiline
-        maxRows={4}
-        fullWidth
-        label="Create Post"
-        variant="standard"
-      />
-      <Button variant="contained" fullWidth sx={{ marginTop: "3rem" }}>
-        Save Post
-      </Button>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        <Form>
+          <Field
+            as={TextField}
+            multiline
+            maxRows={4}
+            fullWidth
+            label="Create Post"
+            variant="standard"
+            name="text"
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            fullWidth
+            sx={{ marginTop: "3rem" }}
+          >
+            Save Post
+          </Button>
+        </Form>
+      </Formik>
     </Card>
   );
 }
