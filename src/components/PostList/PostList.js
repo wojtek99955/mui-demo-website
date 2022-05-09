005f5d48d08b0be8935e7a0b5a95882ba127a126
Wojtek Książek <wojtek99955@gmail.com>
2022-05-09 18:21:15 +0200

    PostsList Posts state added

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,9 +1,30 @@
 import { useContext } from "react";
 import { Context } from "../../ContextProvider";
+import { Card, Button, Typography, Stack } from "@mui/material";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import styled from "styled-components";
+
+const UserIcon = styled(AccountCircleOutlinedIcon)``;
 
 function PostList() {
   const ctx = useContext(Context);
-  return <>PostList</>;
+  const { posts } = ctx;
+
+  return (
+    <>
+      {posts.map((post) => {
+        return (
+          <Card>
+            <Stack direction="row" spacing={2}>
+              <UserIcon />
+              <Typography variant="h6">{`${post.name} ${post.surname}`}</Typography>
+            </Stack>
+            <Typography variant="p">{post.text}</Typography>
+          </Card>
+        );
+      })}
+    </>
+  );
 }
 
 export default PostList;
