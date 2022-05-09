ced1d9a0c2117eb749880d70dc4ad1e16b698dff
Wojtek Książek <wojtek99955@gmail.com>
2022-05-09 19:04:10 +0200

    PostList more toolbar added

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,14 +1,32 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "../../ContextProvider";
-import { Card, Button, Typography, Stack } from "@mui/material";
+import {
+  Card,
+  Button,
+  Typography,
+  Stack,
+  Toolbar,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import styled from "styled-components";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 const UserIcon = styled(AccountCircleOutlinedIcon)``;
 
 function PostList() {
   const ctx = useContext(Context);
   const { posts } = ctx;
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <>
@@ -16,9 +34,19 @@ function PostList() {
         {posts.map((post) => {
           return (
             <Card sx={{ padding: "1rem" }}>
-              <Stack direction="row" spacing={2} mb={3}>
-                <UserIcon />
-                <Typography variant="h6">{`${post.name} ${post.surname}`}</Typography>
+              <Stack direction="row">
+                <Stack direction="row" spacing={2} mb={3}>
+                  <UserIcon />
+                  <Typography variant="h6">{`${post.name} ${post.surname}`}</Typography>
+                </Stack>
+                <Toolbar sx={{ ml: "auto" }}>
+                  <IconButton onClick={handleClick}>
+                    <MoreHorizIcon />
+                  </IconButton>
+                  <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+                    <MenuItem onClick={handleClose}>Delete</MenuItem>
+                  </Menu>
+                </Toolbar>
               </Stack>
               <Typography variant="p">{post.text}</Typography>
             </Card>
