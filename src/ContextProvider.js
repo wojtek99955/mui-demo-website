07a4747ec7cbb393665dcca55667e9cd4e12c24b
Wojtek Książek <wojtek99955@gmail.com>
2022-05-09 17:44:20 +0200

    Posts state added

--- src/ContextProvider.js
+++ src/ContextProvider.js
@@ -4,8 +4,9 @@ export const Context = createContext({});
 
 function ContextProvider({ children }) {
   const [userData, setUserData] = useState({});
+  const [posts, setPosts] = useState([]);
   return (
-    <Context.Provider value={{ userData, setUserData }} r>
+    <Context.Provider value={{ userData, setUserData, posts, setPosts }}>
       {children}
     </Context.Provider>
   );
