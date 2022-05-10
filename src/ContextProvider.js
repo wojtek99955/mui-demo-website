e1f4f558a64579394b52b02c903a4cd4be316678
Wojtek Książek <wojtek99955@gmail.com>
2022-05-10 16:51:23 +0200

    saved posts state

--- src/ContextProvider.js
+++ src/ContextProvider.js
@@ -5,8 +5,18 @@ export const Context = createContext({});
 function ContextProvider({ children }) {
   const [userData, setUserData] = useState({});
   const [posts, setPosts] = useState([]);
+  const [savedPosts, setSavedPosts] = useState([]);
   return (
-    <Context.Provider value={{ userData, setUserData, posts, setPosts }}>
+    <Context.Provider
+      value={{
+        userData,
+        setUserData,
+        posts,
+        setPosts,
+        savedPosts,
+        setSavedPosts,
+      }}
+    >
       {children}
     </Context.Provider>
   );
