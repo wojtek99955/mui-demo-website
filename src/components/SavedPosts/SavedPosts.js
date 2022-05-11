598f0a33f9742f70722a0224f485ddde354393c8
Wojtek Książek <wojtek99955@gmail.com>
2022-05-11 14:44:19 +0200

    Saved Posts no post message

--- src/components/SavedPosts/SavedPosts.js
+++ src/components/SavedPosts/SavedPosts.js
@@ -1,16 +1,26 @@
 import { Context } from "../../ContextProvider";
 import { useContext } from "react";
+import styled from "styled-components";
 
+const Container = styled.section`
+  width: 100%;
+  h1 {
+    text-align: center;
+    margin: 2rem 0;
+    font-size: 2rem;
+  }
+`;
 function SavedPosts() {
   const ctx = useContext(Context);
   const likedPosts = ctx.posts.filter((notes) => notes.liked === true);
 
   return (
-    <div>
+    <Container>
+      {likedPosts.length === 0 ? <h1>No saved posts here!</h1> : null}
       {likedPosts.map((post) => {
         return <h1>{post.text}</h1>;
       })}
-    </div>
+    </Container>
   );
 }
 
