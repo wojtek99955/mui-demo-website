6dde63ff9f56bbce52f7ff379e1e18656d8efcaf
Wojtek Książek <wojtek99955@gmail.com>
2022-05-14 15:20:24 +0200

    grid added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Button, Typography, Stack, Card, Divider } from "@mui/material";
+import { Button, Typography, Stack, Card, Divider, Grid } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { Context } from "../../ContextProvider";
@@ -18,26 +18,43 @@ function Dashboard() {
     userData: { fname },
   } = context;
   return (
-    <div>
-      <Card
-        sx={{
-          width: "100%",
-          marginTop: "1rem",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant="h4" component="h1" align="center">
-          Welcome {fname}
-        </Typography>
-        <Avatar sx={{ fontSize: "5rem" }} />
-        <Divider />
-        <StyledLink to="/profile">
-          <Button>My profile</Button>
-        </StyledLink>
-      </Card>
-    </div>
+    <Grid container spacing={3} sx={{ padding: "3rem" }}>
+      <Grid item xs={12} md={6}>
+        <Card
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          <Typography variant="h4" component="h1" align="center">
+            Welcome {fname}
+          </Typography>
+          <Avatar sx={{ fontSize: "5rem" }} />
+          <Divider />
+          <StyledLink to="/profile">
+            <Button>My profile</Button>
+          </StyledLink>
+        </Card>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Card sx={{ height: "100%" }}>
+          <Button>Add Post</Button>
+        </Card>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Card sx={{ height: "100%" }}>
+          <h3>Posts</h3>
+        </Card>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Card sx={{ height: "100%" }}>
+          <h3>Card</h3>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
 
