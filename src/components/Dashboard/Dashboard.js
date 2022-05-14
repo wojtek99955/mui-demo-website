6f2ecf714c8172907939c19f2468bf2721b1ded9
Wojtek Książek <wojtek99955@gmail.com>
2022-05-14 13:57:09 +0200

    Profile Card added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,33 @@
+import styled from "styled-components";
+import { Box, Typography, Stack, Card, Divider } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "../../ContextProvider";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+const Avatar = styled(AccountCircleOutlinedIcon)`
+  width: 2rem;
+`;
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const context = useContext(Context);
+  const {
+    userData: { fname },
+  } = context;
+  return (
+    <div>
+      <Card sx={{ width: "100%", border: "1px solid red", marginTop: "1rem" }}>
+        <Typography variant="h4" component="h1" align="center">
+          Welcome {fname}
+        </Typography>
+        <Avatar sx={{ fontSize: "5rem" }} />
+        <Divider />
+      </Card>
+    </div>
+  );
 }
 
 export default Dashboard;
