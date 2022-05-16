8f58c7655926d8f310f36e00b13db9f8fa0ba59d
Wojtek Książek <wojtek99955@gmail.com>
2022-05-16 15:21:02 +0200

    use navigate added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -18,14 +18,12 @@ import { Context } from "../../ContextProvider";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import SettingsIcon from "@mui/icons-material/Settings";
 import NotesIcon from "@mui/icons-material/Notes";
+import { useNavigate } from "react-router-dom";
 
 const StyledSettingsIcon = styled(SettingsIcon)`
   color: grey;
 `;
 
-const StyledLink = styled(Link)`
-  text-decoration: none;
-`;
 const AvatarIcon = styled(AccountCircleOutlinedIcon)``;
 
 function Dashboard() {
@@ -34,6 +32,8 @@ function Dashboard() {
     userData: { fname },
   } = context;
   const { posts } = context;
+
+  let navigate = useNavigate();
 
   return (
     <Grid container spacing={3} sx={{ padding: "3rem", height: "100%" }}>
@@ -63,14 +63,18 @@ function Dashboard() {
               <ListItemText primary={`posts: ${posts.length}`} />
             </ListItem>
           </List>
-          <StyledLink to="/profile">
-            <Button variant="outlined">My profile</Button>
-          </StyledLink>
+          <Button variant="outlined" onClick={() => navigate("/profile")}>
+            My profile
+          </Button>
         </Card>
       </Grid>
       <Grid item xs={12} md={6}>
         <Card sx={{ height: "100%", display: "flex" }}>
-          <Button variant="contained" sx={{ margin: "auto" }}>
+          <Button
+            variant="contained"
+            sx={{ margin: "auto" }}
+            onClick={() => navigate("/posts")}
+          >
             Add Post
           </Button>
         </Card>
@@ -92,11 +96,16 @@ function Dashboard() {
               })}
             </Grid>
           </ul>
-          <Button variant="outlined">More</Button>
+          <Button variant="outlined" onClick={() => navigate("/posts")}>
+            More
+          </Button>
         </Card>
       </Grid>
       <Grid item xs={12} md={6}>
-        <Card sx={{ height: "100%", display: "flex" }}>
+        <Card
+          sx={{ height: "100%", display: "flex", cursor: "pointer" }}
+          onClick={() => navigate("/settings")}
+        >
           <StyledSettingsIcon sx={{ fontSize: "5rem", margin: "auto" }} />
           <Typography variant="h3" component="h3" sx={{ margin: "auto" }}>
             Settings
