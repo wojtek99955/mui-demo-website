123361ed83a9a21a93179132c604c98af68fb263
Wojtek Książek <wojtek99955@gmail.com>
2022-05-17 14:44:42 +0200

    styles update

--- src/components/SavedPosts/SavedPosts.js
+++ src/components/SavedPosts/SavedPosts.js
@@ -1,7 +1,7 @@
 import { Context } from "../../ContextProvider";
 import { useContext } from "react";
 import Post from "../Post/Post";
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 function SavedPosts() {
   const ctx = useContext(Context);
@@ -9,7 +9,11 @@ function SavedPosts() {
 
   return (
     <Stack spacing={3} sx={{ minWidth: "550px", margin: " 1rem auto" }}>
-      {likedPosts.length === 0 ? <h1>No saved posts here!</h1> : null}
+      {likedPosts.length === 0 ? (
+        <Typography variant="h4" component="h1" align="center">
+          No saved posts here!
+        </Typography>
+      ) : null}
       {likedPosts.map((post) => {
         return <Post post={post} />;
       })}
