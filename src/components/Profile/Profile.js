1d34fed6588abb165075f961bc4998730779b8f9
Wojtek Książek <wojtek99955@gmail.com>
2022-05-18 18:49:09 +0200

    layout changed

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,13 +1,17 @@
 import styled from "styled-components";
-import { Box, Typography } from "@mui/material";
-import { Button } from "@mui/material";
-import { Stack } from "@mui/material";
-import { Link } from "react-router-dom";
-import { Card } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Container,
+  Button,
+  Stack,
+  Link,
+  Card,
+  Divider,
+} from "@mui/material";
 import { useContext } from "react";
 import { Context } from "../../ContextProvider";
-
-const Container = styled.section``;
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
 const StyledLink = styled(Link)`
   text-decoration: none;
@@ -16,30 +20,48 @@ const StyledLink = styled(Link)`
 function Profile() {
   const context = useContext(Context);
   const {
-    userData: { fname, lname, address, zipCode, city },
+    userData: { fname, lname, address, zipCode, city, email },
   } = context;
   return (
-    <Card sx={{ maxWidth: "600px", margin: "auto", marginTop: "1rem" }}>
-      <Typography variant="h3" component="h1" align="center">
-        Your Profile
-      </Typography>
-      <Stack direction="row" justifyContent="space-around">
-        <Typography>{fname}</Typography>
-        <Typography>{lname}</Typography>
-      </Stack>
-      <Stack direction="row" justifyContent="space-around">
-        <Typography>{address}</Typography>
-      </Stack>
-      <Stack direction="row" justifyContent="space-around">
-        <Typography>{zipCode}</Typography>
-        <Typography>{city}</Typography>
-      </Stack>
-      <Box textAlign="center">
-        <StyledLink to="/edit-profile">
-          <Button variant="contained">Update</Button>
-        </StyledLink>
-      </Box>
-    </Card>
+    <>
+      <Container>
+        <Typography variant="h3" component="h1" align="center">
+          Your Profile
+        </Typography>
+        <Card
+          sx={{
+            maxWidth: "600px",
+            margin: "auto",
+            marginTop: "1rem",
+            padding: "1rem",
+          }}
+        >
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <AccountCircleOutlinedIcon
+              sx={{ fontSize: "5rem", margin: "auto" }}
+            />
+          </Box>
+          <Typography variant="h4" align="center">
+            {fname} {lname}
+          </Typography>
+          <Typography variant="h6" align="center">
+            {email}
+          </Typography>
+          <Stack direction="row" justifyContent="space-around">
+            <Typography>{address}</Typography>
+          </Stack>
+          <Stack direction="row" justifyContent="space-around">
+            <Typography>{zipCode}</Typography>
+            <Typography>{city}</Typography>
+          </Stack>
+          <Box textAlign="center">
+            <StyledLink to="/edit-profile">
+              <Button variant="contained">Update</Button>
+            </StyledLink>
+          </Box>
+        </Card>
+      </Container>
+    </>
   );
 }
 
