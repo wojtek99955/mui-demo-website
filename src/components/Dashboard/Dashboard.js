6214ac22e911d3f4559d37584d02b61521517158
Wojtek Książek <wojtek99955@gmail.com>
2022-05-18 22:34:35 +0200

    grid item update

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -7,16 +7,16 @@ import SettingsCard from "./SettingsCard/SettingsCard";
 function Dashboard() {
   return (
     <Grid container spacing={3} sx={{ padding: "3rem", height: "100%" }}>
-      <Grid item xs={12} md={6}>
+      <Grid item xs={12} md={6} sx={{ minHeight: "22rem" }}>
         <ProfileCard />
       </Grid>
-      <Grid item xs={12} md={6}>
+      <Grid item xs={12} md={6} sx={{ minHeight: "22rem" }}>
         <AddPostCard />
       </Grid>
-      <Grid item xs={12} md={6}>
+      <Grid item xs={12} md={6} sx={{ minHeight: "22rem" }}>
         <RecentPosts />
       </Grid>
-      <Grid item xs={12} md={6}>
+      <Grid item xs={12} md={6} sx={{ minHeight: "22rem" }}>
         <SettingsCard />
       </Grid>
     </Grid>
