ad1d10aa585d6cedb357302aad5d041d6d2eaf37
Wojtek Książek <wojtek99955@gmail.com>
2022-05-20 13:20:35 +0200

    layout changed

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,18 +1,21 @@
-import { Card, Typography, Switch, Container } from "@mui/material";
+import { Card, Typography, Stack, Container } from "@mui/material";
 import { Link, Outlet } from "react-router-dom";
 
 function Settings() {
   return (
     <Container>
+      <Typography variant="h4" align="center">
+        Settings
+      </Typography>
       <Card sx={{ margin: " 1rem auto", maxWidth: "800px", padding: "1rem" }}>
-        <Typography variant="h4" align="center">
-          Settings
-        </Typography>
-        <Link to="/settings/preferences">
-          <Typography variant="h5">Account</Typography>
-        </Link>
-        <Typography variant="h5">Preferences</Typography>
-        <Outlet />
+        <Stack direction="row" spacing={2}>
+          <Stack>
+            <Link to="/settings/preferences">
+              <Typography variant="h5">Preferences</Typography>
+            </Link>
+          </Stack>
+          <Outlet />
+        </Stack>
       </Card>
     </Container>
   );
